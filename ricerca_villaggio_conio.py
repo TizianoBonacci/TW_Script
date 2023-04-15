b2ce0992903c9f9ebb143ff2eb2b5df7d1ed9963
Tiziano <tizianob100@gmail.com>
2023-04-15 13:17:00 +0000

    fix estrazione coordinate nomi e data visualization

--- ricerca_villaggio_conio.py
+++ ricerca_villaggio_conio.py
@@ -2,7 +2,7 @@ import re
 from itertools import combinations
 import math
 from typing import Tuple, List
-
+from tabulate import tabulate
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,30 +13,31 @@ def distance(coord1: Tuple[int, int], coord2: Tuple[int, int]) -> float:
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-def find_best_villages(input_string: str, n: int = 5) -> List[Tuple[str, Tuple[int, int]]]:
-    lines = input_string.split('\n')
+def find_best_villages(input_string, n=5):
+    lines = input_string.strip().split('\n')
     village_data = []
 
-    for line in lines:
-        coords_match = re.search(r'(\d+\|\d+)', line)
-        if coords_match:
-            coords_str = coords_match.group()
-            coords = tuple(map(int, coords_str.split('|')))
-            village_name = re.search(r'^(.+?)\s*\d+\|\d+', line).group(1).strip()
-            village_data.append((village_name, coords))
+    for line in lines[3:]:
+        if '|' in line:
+            name, coords = line[:-7], line[-7:]
+            x, y = coords.split('|')
+            village_data.append((name, (int(x), int(y))))
+
+    distances = []
+    for i, village1 in enumerate(village_data):
+        for j, village2 in enumerate(village_data[i + 1:]):
+            dist = distance(village1[1], village2[1])
+            distances.append((village1, village2, dist))
 
     avg_distances = []
+    for village in village_data:
+        total_distance = sum(dist for v1, v2, dist in distances if village in (v1, v2))
+        num_connections = sum(1 for v1, v2, dist in distances if village in (v1, v2))
+        avg_distance = total_distance / num_connections
+        avg_distances.append((village, avg_distance))
 
-    for i, (name1, coords1) in enumerate(village_data):
-        total_distance = sum(distance(coords1, coords2) for _, coords2 in village_data if coords1 != coords2)
-        avg_distance = total_distance / (len(village_data) - 1)
-        avg_distances.append((avg_distance, name1, coords1))
-
-    avg_distances.sort()
-    best_villages = [(name, coords) for _, name, coords in avg_distances[:n]]
-
-    return best_villages
-
+    avg_distances.sort(key=lambda x: x[1])
+    return avg_distances[:n]
 
 
 def IO_ricerca_villaggio_conio():
@@ -58,12 +59,16 @@ def IO_ricerca_villaggio_conio():
       print(f"Errore durante il recupero della pagina. Codice di stato: {response.status_code}")
   
   
-  best_villages = find_best_villages(villages_list)
+  best_villages = find_best_villages(villages_list, 5)
   print("\n")
   print("Questi sono i migliori villaggi in cui coniare per distanza tra i restanti villaggi:")
-  for name, coords in best_villages:
-      print(f"{name} {coords}")
+  table_data = []
+  for village, avg_distance in best_villages:
+      table_data.append([village[0], village[1][0], village[1][1], f"{avg_distance:.3f}"])
+  
+  # Creiamo la tabella usando tabulate
+  table = tabulate(table_data, headers=["Village Name", "X", "Y", "Avg Distance"],   tablefmt="pretty")
+  print(table)
   print("\n")
-
   
 
